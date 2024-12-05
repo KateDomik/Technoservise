3a2843d4a512edbcb8cbd996d27410d14c9a6337
KateDomik <120799719+KateDomik@users.noreply.github.com>
2024-12-05 15:41:52 +0300

    Add files via upload

---
+++ ООО_Техносервис/Classes/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООО_Техносервис.Classes
+{
+    //Класс с глобальными величинами
+    public class Helper
+    {
+        // BD connection
+        public static Model.DBDomayskaya21Entities DBRequest { get; set; } // static означает, что объект будет в единственном экземпляре
+
+        //
+        public static Model.User user;
+    }
+}
